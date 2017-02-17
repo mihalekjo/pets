41a2542502be81cc94c702fb67a3f41267fcdc69
mihalekjo <17mihalekjo@fpsct.org>
2017-02-17 09:53:35 -0500

    Joe Mihalek's dog
    Dog of Joe Mihalek

---
+++ Dog.java
@@ -0,0 +1,16 @@
+public class Dog extends Pet
+{
+    public Dog()
+    { 
+        super("fido");
+    }
+    public Dog(String theName)
+    {
+        super(theName);
+    }
+    public String speak()
+    {
+        return "bark";
+    }
+}  
+        
